5c28154d25fc33d3bfd494b4e0032ab0111f9cfc
Abdullahi Usman <abdullahismn7@gmail.com>
2023-11-03 04:56:04 +0100

    Initial commit

---
+++ app/src/main/java/com/khemicalkoder/room260ksptest/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.khemicalkoder.room260ksptest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.khemicalkoder.room260ksptest.ui.theme.Room260KSPTestTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Room260KSPTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Room260KSPTestTheme {
+        Greeting("Android")
+    }
+}
